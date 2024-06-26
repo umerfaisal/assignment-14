f4c1b8a8252bd8f1fff02250312c9546f8d92058
umerfaisal <109797391+umerfaisal@users.noreply.github.com>
2024-06-26 16:52:52 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+var guestList = ["Umer", "Mubeen", "Huzaifa", "Areeb"];
+guestList.forEach(function (oneGuest) { return console.log("hello ".concat(oneGuest, ", Would you like to dinner with me? ")); });
---
+++ main.ts
@@ -0,0 +1,2 @@
+let guestList = ["Umer","Mubeen","Huzaifa","Areeb"]
+guestList.forEach(oneGuest => console.log(`hello ${oneGuest}, Would you like to dinner with me? `))
